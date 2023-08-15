0ae024ca2cbc7b7f11c6e225f9de0d665e0b193f
Chathurika Gajanayaka <chathurikagajanayaka@d0:88:0c:87:0a:70.home>
2023-08-16 00:47:23 +0200

    Add requirements for deployment

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 # Your code goes here.
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
+
 
 
 
@@ -24,7 +24,7 @@ def get_sales_data():
     print("Data should be six numbers, separated by commas.")
     print("Example: 10,20,30,40,50,60")
 
-    data_str = input("Enter your data here: ")
+    data_str = input("Enter your data here:\n ")
 
     sales_data = data_str.split(",")
 
